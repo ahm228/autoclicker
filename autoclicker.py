51f09befbbb52e82294cc15d49ccaccc9dd7680f
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-11-10 20:24:33 -0600

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -1,15 +1,13 @@
 import pyautogui
 import time
 import threading
-import keyboard  # Requires the 'keyboard' library, which can be installed using 'pip install keyboard'
+import keyboard
 
-# Configuration
-click_interval = 0.1  # Interval between clicks, in seconds
-button = 'left'  # The mouse button to click, can be 'left', 'right' or 'middle'
-start_stop_key = 'f6'  # The key to start/stop the autoclicker
-exit_key = 'f10'  # The key for exiting the script
+click_interval = 0.00001  #Interval between clicks, in seconds
+button = 'left'  #The mouse button to click, can be 'left', 'right' or 'middle'
+start_stop_key = 'f6'  #The key to start/stop the script
+exit_key = 'f10'  #The key for exiting the script
 
-# Variable to keep track of the clicker state
 clicking = False
 
 def clicker():
@@ -27,16 +25,22 @@ def toggle_autoclicker():
         print("Autoclicker stopped. Press F6 to start.")
 
 def exit_autoclicker():
+    global clicking
+    clicking = False
     print("Exiting autoclicker.")
-    listener.stop()
+    click_thread.join()  #Wait for the click_thread to finish
+    exit()
 
-# Setup hotkeys
 keyboard.add_hotkey(start_stop_key, toggle_autoclicker)
 keyboard.add_hotkey(exit_key, exit_autoclicker)
 
-# Create and start the clicker thread
 click_thread = threading.Thread(target=clicker)
 click_thread.start()
 
-print("Autoclicker running (press F6 to start/stop, F10 to exit).")
-listener = keyboard.wait(exit_key)
+try:
+    print("Script running (press F6 to start/stop, F10 to exit).")
+    keyboard.wait(exit_key)
+except KeyboardInterrupt:
+    print("Exiting script.")
+    click_thread.join()  #Ensure that the clicker thread is properly terminated
+    exit()
